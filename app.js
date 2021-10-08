daf30d607141b4ecfb0e8f524b396f354fd6d37b
Abdulkerim Hıcıp <abdulkerimhcp@gmail.com>
2021-10-08 21:09:54 +0300

    express and nodemon install

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = () => {
-  console.log('function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  const photo = {
+    id: 1,
+    name: "Photo Name",
+    description: "Photo description"
+  }
+  res.send(photo)
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu http://localhost:${port} başlatıldı..`);
+});
