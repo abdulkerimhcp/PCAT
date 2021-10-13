f21818c649bf3e673515115d81cc2898cccbd00d
Abdulkerim Hıcıp <abdulkerimhcp@gmail.com>
2021-10-13 19:00:00 +0300

    photo models created and photos list index.ejs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/pcat-db');
 
 const app = express();
 
@@ -8,16 +12,26 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index')
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
 });
 app.get('/about', (req, res) => {
-  res.render('about')
+  res.render('about');
 });
 app.get('/add', (req, res) => {
-  res.render('add')
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
